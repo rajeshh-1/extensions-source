08c2198eb2b3095faef10a7fcb3d12b345a12b16
Gabriel Stangari <56012007+rajeshh-1@users.noreply.github.com>
2026-07-13 09:42:11 -0300

    chore: bump versionCode to 70 (v13)

--- src/pt/mangalivre/build.gradle.kts
+++ src/pt/mangalivre/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Manga Livre"
     className = "MangaLivre"
-    versionCode = 69
+    versionCode = 70
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 }
