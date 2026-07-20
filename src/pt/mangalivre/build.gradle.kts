15758d58bdaa7e88ff5e33ad800d99c300ee26b0
Gabriel Stangari <56012007+rajeshh-1@users.noreply.github.com>
2026-07-20 11:24:40 -0300

    MangaLivre: follow rotated ToonLivre signature header

--- src/pt/mangalivre/build.gradle.kts
+++ src/pt/mangalivre/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 keiyoushi {
     name = "Manga Livre"
     className = "MangaLivre"
-    versionCode = 82
+    versionCode = 83
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 }
